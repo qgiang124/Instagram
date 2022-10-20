ca778242ef58bb740972f8672b078609e5cebc2a
Giang Tran <giangtran2@my.unt.edu>
2022-10-20 16:31:18 -0500

    update comments

---
+++ Instagram/PosterCell.swift
@@ -0,0 +1,23 @@
+//
+//  PosterCell.swift
+//  Instagram
+//
+//  Created by Giang Tran on 10/14/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
