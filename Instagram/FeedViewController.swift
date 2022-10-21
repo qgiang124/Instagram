3e55c7f36075f4375d7cc4ac7115df019c1deb33
Giang Tran <giangtran2@my.unt.edu>
2022-10-21 02:07:58 -0500

    fix constraints

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -18,6 +18,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     let commentBar = MessageInputBar()
     var showsCommentBar = false
     var posts = [PFObject]()
+    var selectedPost: PFObject!
     
     override var inputAccessoryView: UIView? {
         return commentBar
@@ -69,6 +70,22 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
         //Create the comment
+        let comment = PFObject(className: "Comments")
+        comment["text"] = text
+        comment["post"] = selectedPost
+        comment["author"] = PFUser.current()!
+        
+        selectedPost.add(comment, forKey: "comments")
+        
+        selectedPost.saveInBackground{ (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+        
+        tableView.reloadData()
         
         //Clear and dismiss the input bar
         commentBar.inputTextView.text = nil
@@ -96,7 +113,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         } else if indexPath.row <= comments.count {
             let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
             let comment = comments[indexPath.row - 1]
-            cell.userNameLabel.text = comment["text"] as? String
+            cell.commentLabel.text = comment["text"] as? String
             let user = comment["author"] as! PFUser
             cell.userNameLabel.text = user.username
             return cell
@@ -137,6 +154,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             showsCommentBar = true
             becomeFirstResponder()
             commentBar.inputTextView.becomeFirstResponder()
+            
+            selectedPost = post
         }
     }
 }
